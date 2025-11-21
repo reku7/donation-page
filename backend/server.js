7f22ccf0c3f3420bf5357c79e3f1e7b98088e58c
reku7 <rekiklegese@gmail.com>
2025-11-20 21:56:00 -0800

    Moved nodeSDK inside backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import SantimpaySdk from "../nodeSDK/lib/index.js";
+import SantimpaySdk from "./nodeSDK/lib/index.js";
 
 dotenv.config();
 
